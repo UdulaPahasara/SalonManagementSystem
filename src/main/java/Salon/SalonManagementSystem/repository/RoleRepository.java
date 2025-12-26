646f6a116ce059afac50feb279238f8af8fcf02b
UdulaPahasara <udulapahasara2@gmail.com>
2025-12-26 10:03:44 -0800

    find Role by RoleName

--- src/main/java/Salon/SalonManagementSystem/repository/RoleRepository.java
+++ src/main/java/Salon/SalonManagementSystem/repository/RoleRepository.java
@@ -4,5 +4,5 @@ import Salon.SalonManagementSystem.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-
+    java.util.Optional<Role> findByRoleName(String roleName);
 }
